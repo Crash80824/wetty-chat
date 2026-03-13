526c7dfceb5f7b2404196061647a8de4b2a39b8b
Codetector <codetector@codetector.org>
2026-03-12 17:53:41 -0700

    frontend/misc: Remove lingui line number comments

--- wetty-chat-mobile/lingui.config.ts
+++ wetty-chat-mobile/lingui.config.ts
@@ -1,4 +1,5 @@
 import type { LinguiConfig } from "@lingui/conf";
+import { formatter } from '@lingui/format-po';
 
 const config: LinguiConfig = {
   sourceLocale: "en",
@@ -9,6 +10,9 @@ const config: LinguiConfig = {
       include: ["src"],
     },
   ],
+  format: formatter({
+    lineNumbers: false
+  })
 };
 
 export default config;
