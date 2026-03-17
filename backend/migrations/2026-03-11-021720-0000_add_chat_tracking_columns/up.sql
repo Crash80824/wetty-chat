d60f3d43e651e957f736a8188560a3592dc17916
Codetector <codetector@codetector.org>
2026-03-16 23:18:40 -0700

    patch chat to drop the fkey

--- backend/migrations/2026-03-11-021720-0000_add_chat_tracking_columns/up.sql
+++ backend/migrations/2026-03-11-021720-0000_add_chat_tracking_columns/up.sql
@@ -7,7 +7,7 @@ SET last_message_id = (SELECT id FROM messages WHERE chat_id = groups.id ORDER B
     last_message_at = (SELECT created_at FROM messages WHERE chat_id = groups.id ORDER BY created_at DESC LIMIT 1);
 
 -- For tracking unread messages
-ALTER TABLE group_membership ADD COLUMN last_read_message_id BIGINT REFERENCES messages(id);
+ALTER TABLE group_membership ADD COLUMN last_read_message_id BIGINT;
 
 -- Initialize last_read_message_id for existing memberships to the latest message, so they don't suddenly have thousands of unread messages.
 UPDATE group_membership
